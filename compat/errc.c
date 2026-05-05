536d1bdfad23ed281e18f96701ede7f20ef4e9e5
250MHz <95242017+250MHz@users.noreply.github.com>
2026-05-05 12:41:30 -0700

    Add compat static library
    * arc4random and strtonum from Portable OpenSSH
    
    * errc is from OpenSMTPD portable
    
    * progname is from FreeBSD
    
    * queue is from OpenBSD

---
+++ compat/errc.c
@@ -0,0 +1,167 @@
+/*
+ * Copyright (c) 2023 Omar Polo <op@openbsd.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+#include "compat.h"
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef HAVE_VWARN
+void
+vwarn(const char *fmt, va_list ap)
+{
+	int save_errno;
+
+	save_errno = errno;
+
+	fprintf(stderr, "%s: ", dtnc_getprogname());
+	if (fmt != NULL) {
+		vfprintf(stderr, fmt, ap);
+		fprintf(stderr, ": ");
+	}
+	fprintf(stderr, "%s\n", strerror(save_errno));
+
+	errno = save_errno;
+}
+#endif
+
+#ifndef HAVE_VWARNC
+void
+vwarnc(int code, const char *fmt, va_list ap)
+{
+	errno = code;
+	vwarn(fmt, ap);
+}
+#endif
+
+#ifndef HAVE_VWARNX
+void
+vwarnx(const char *fmt, va_list ap)
+{
+	int save_errno;
+
+	save_errno = errno;
+
+	fprintf(stderr, "%s: ", dtnc_getprogname());
+	if (fmt != NULL)
+		vfprintf(stderr, fmt, ap);
+	fputc('\n', stderr);
+
+	errno = save_errno;
+}
+#endif
+
+#ifndef HAVE_VERR
+_Noreturn void
+verr(int eval, const char *fmt, va_list ap)
+{
+	vwarn(fmt, ap);
+	exit(eval);
+}
+#endif
+
+#ifndef HAVE_VERRC
+_Noreturn void
+verrc(int eval, int code, const char *fmt, va_list ap)
+{
+	vwarnc(code, fmt, ap);
+	exit(eval);
+}
+#endif
+
+#ifndef HAVE_VERRX
+_Noreturn void
+verrx(int eval, const char *fmt, va_list ap)
+{
+	vwarnx(fmt, ap);
+	exit(eval);
+}
+#endif
+
+#ifndef HAVE_ERR
+_Noreturn void
+err(int eval, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	verr(eval, fmt, ap);
+	va_end(ap);
+}
+#endif
+
+#ifndef HAVE_ERRC
+_Noreturn void
+errc(int eval, int code, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	verrc(eval, code, fmt, ap);
+	va_end(ap);
+}
+#endif
+
+#ifndef HAVE_ERRX
+_Noreturn void
+errx(int eval, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	verrx(eval, fmt, ap);
+	va_end(ap);
+}
+#endif
+
+#ifndef HAVE_WARN
+void
+warn(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vwarn(fmt, ap);
+	va_end(ap);
+}
+#endif
+
+#ifndef HAVE_WARNC
+void
+warnc(int code, const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vwarnc(code, fmt, ap);
+	va_end(ap);
+}
+#endif
+
+#ifndef HAVE_WARNX
+void
+warnx(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vwarnx(fmt, ap);
+	va_end(ap);
+}
+#endif
